eab0f876c1a1cc331ea7a8d312ce025f281e8a2e
bryan-cortes <cortesbryan32@yahoo.com>
2020-11-13 14:38:19 -0500

    completed summation

--- index.js
+++ index.js
@@ -25,17 +25,20 @@ myFunction();
 /* 🚀🚀🚀 Task 2: Counter 🚀🚀🚀 */
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 function summation(number) {
+
   const arrNum= [];
 
   for(let i = 0 ; i <= number ;i++){
-    arrNum.push(i)
+    arrNum.push(i);
   }
 
-  const total = arrNum.reduce(function(accu, item,index){
-    return accu + item[index.length];
+  const total = arrNum.reduce(function(accu, item){
+    return accu + item;
 
   },0);
-    
+  
+    return total
+  
 }
 
 console.log(summation(4));
